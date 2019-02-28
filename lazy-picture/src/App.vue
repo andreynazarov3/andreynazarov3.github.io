ec1ea7985ec1c92b1cda09bfbcd3b2daa09ad6bf
Andrey Nazarov <andreynazarov3@yandex.ru>
2019-02-28 22:03:16 +0400

    build 2

--- lazy-picture/src/App.vue
+++ lazy-picture/src/App.vue
@@ -1,38 +1,71 @@
 <template>
-	<div id="app">
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-		<lazy-picture :sources="sources" :lazy="true"></lazy-picture>
-	</div>
+  <div id="app">
+    <h1>Lazy loading demo</h1>
+    <p><em>Resize window to see how it works!</em></p>
+    <h2>Pic 1: without fallback</h2>
+    <p>Data used:</p>
+    <pre>
+    &lt;lazy-picture :picture-sources="sources[0].picture" lazy&gt;
+        
+    {
+        picture: [
+          {
+            media: "(max-width: 480px)",
+            srcset: "./images/New_york_times_square-terabass_480@1x.jpg, ./images/New_york_times_square-terabass_480@2x.jpg 2x",
+            placeholder:
+              "./images/New_york_times_square-terabass_480@0x.jpg"
+          },
+          {
+            media: "(min-width: 481px)",
+            srcset: "./images/New_york_times_square-terabass.jpg",
+            placeholder:
+              "./images/rsz_new_york_times_square-terabass-small.jpg"
+          }
+        ]
+      }
+    </pre>
+    <lazy-picture :picture-sources="sources[0].picture" lazy />
+  </div>
 </template>
 
 <script>
 import lazyPicture from "./components/LazyPicture.vue";
 
 export default {
-	name: "app",
-	components: {
-		lazyPicture
-	},
-	data() {
-		return {
-			sources: [
-				{
-					fields: {
-						src: "./images/New_york_times_square-terabass.jpg",
-						placeholder: "./images/rsz_new_york_times_square-terabass-small.jpg"
-					},
-					sys: {
-						id: "1"
-					}
-				}
-			]
-		};
-	}
+  name: "app",
+  components: {
+    lazyPicture
+  },
+  data() {
+    return {
+      sources: [
+        {
+          // img: {
+          //   src: "./images/New_york_times_square-terabass.jpg",
+          //   placeholder: "./images/rsz_new_york_times_square-terabass-small.jpg"
+          // },
+          picture: [
+            {
+              media: "(max-width: 480px)",
+              srcset:
+                "./images/New_york_times_square-terabass_480@1x.jpg, ./images/New_york_times_square-terabass_480@2x.jpg 2x",
+              placeholder: "./images/New_york_times_square-terabass_480@0x.jpg"
+            },
+            { 
+              media: "(min-width: 481px)",
+              srcset: "./images/New_york_times_square-terabass.jpg",
+              placeholder:
+                "./images/rsz_new_york_times_square-terabass-small.jpg"
+            }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
+<style>
+pre {
+  overflow: auto;
+}
+</style>
